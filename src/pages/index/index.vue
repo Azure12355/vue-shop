8662d8ea7f0d4c07e3f842f90a8e06a91b1c6a6e
Lytton Yang <azure12355@gmail.com>
2024-09-20 19:46:14 +0800

    feat: 新增scroll-view滚动容器

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -35,25 +35,28 @@ onLoad(() => {
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavBar />
-  <!-- 自定义轮播图 -->
-  <XtxSwiper :list="bannerList" />
-  <!-- 分类面板 -->
-  <CategoryPanel :list="categoryList" />
-  <!-- 热门推荐 -->
-  <HotPanel :list="hotList" />
-  <view class="index">index</view>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
+
+  <!-- 滚动容器 -->
+  <scroll-view class="scroll-view" scroll-y>
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢 -->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .scroll-view {
+    flex: 1;
+  }
 }
 </style>
