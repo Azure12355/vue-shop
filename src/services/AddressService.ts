4d9d7f668757c3e9044c414685a2b81e2a452ec2
Lytton Yang <azure12355@gmail.com>
2024-10-16 23:32:11 +0800

    feat: 地址模块-修改地址与表单校验

--- src/services/AddressService.ts
+++ src/services/AddressService.ts
@@ -26,4 +26,28 @@ export default class AddressService {
       url: "/member/address",
     })
   }
+
+  /**
+   * 获取收获地址详情
+   * @param id 地址id
+   */
+  public static getMemberAddressDetailsAPI = (id: string): Promise<Result<AddressItem>> => {
+    return HttpUtil.http<AddressItem>({
+      method: "GET",
+      url: `/member/address/${id}`,
+    })
+  }
+
+  /**
+   * 修改收货地址
+   * @param id 地址id(路径参数)
+   * @param data 表单数据(请求体参数)
+   */
+  public static putMemberAddressByIdAPI = (id: string, data: AddressParams) => {
+    return HttpUtil.http({
+      method: "PUT",
+      url: `/member/address/${id}`,
+      data,
+    })
+  }
 }
