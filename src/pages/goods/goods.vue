231f032a66e7b0ed475eacc3e0d95e78a0da84ce
Lytton Yang <azure12355@gmail.com>
2024-10-17 17:39:19 +0800

    feat: 购物车模块-完成添加到购物车功能

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -6,10 +6,11 @@ import { ref, computed } from "vue"
 import ServicePanel from "./components/ServicePanel.vue"
 import AddressPanel from "./components/AddressPanel.vue"
 import type {
+  SkuPopupEvent,
   SkuPopupInstance,
   SkuPopupLocaldata,
-  SkuPopupSkuItem,
 } from "@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup"
+import CartService from "@/services/CartService"
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -70,12 +71,14 @@ const openPopup = (name: typeof popupName.value) => {
 const isShowSKU = ref(false)
 //商品信息
 const localdata = ref({} as SkuPopupLocaldata)
+
 //SKU按钮模式
 enum SKU_MODE {
   BOTH = 1, //全部显示
   CART = 2, //购物车
   BUY = 3, //购买
 }
+
 const mode = ref<SKU_MODE>(SKU_MODE.CART)
 //打开SKU弹窗修改按钮模式
 const openSkuPopup = (skuMode: SKU_MODE) => {
@@ -90,6 +93,16 @@ const skuPopupRef = ref<SkuPopupInstance>()
 const selectedArrText = computed(() => {
   return skuPopupRef.value?.selectArr?.join(" ").trim() || "请选择商品规格"
 })
+//选定SKU加入购物车
+const onAddCart = async (e: SkuPopupEvent) => {
+  try {
+    await CartService.postMemberCartAPI({ skuId: e._id, count: e.buy_num })
+    uni.showToast({ title: "添加成功" })
+    isShowSKU.value = false
+  } catch (e) {
+    uni.showToast({ icon: "error", title: "添加到购物车失败" })
+  }
+}
 
 onLoad(() => {
   getGoodsData()
@@ -120,8 +133,8 @@ onLoad(() => {
           <text class="symbol">¥</text>
           <text class="number">{{ goods?.price || 0.0 }}</text>
         </view>
-        <view class="name ellipsis">{{ goods?.name || "神秘商品" }} </view>
-        <view class="desc"> {{ goods?.desc || "一件非常有个性的商品" }} </view>
+        <view class="name ellipsis">{{ goods?.name || "神秘商品" }}</view>
+        <view class="desc"> {{ goods?.desc || "一件非常有个性的商品" }}</view>
       </view>
 
       <!--SKU弹窗组件-->
@@ -130,21 +143,22 @@ onLoad(() => {
         :localdata="localdata"
         :mode="mode"
         ref="skuPopupRef"
+        @add-cart="onAddCart"
       />
 
       <!-- 操作面板 -->
       <view class="action">
         <view class="item arrow" @tap="openSkuPopup(SKU_MODE.BOTH)">
           <text class="label">选择</text>
-          <text class="text ellipsis"> {{ selectedArrText }} </text>
+          <text class="text ellipsis"> {{ selectedArrText }}</text>
         </view>
         <view class="item arrow" @tap="openPopup('address')">
           <text class="label">送至</text>
-          <text class="text ellipsis"> 请选择收获地址 </text>
+          <text class="text ellipsis"> 请选择收获地址</text>
         </view>
         <view class="item arrow" @tap="openPopup('service')">
           <text class="label">服务</text>
-          <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
+          <text class="text ellipsis"> 无忧退 快速退款 免费包邮</text>
         </view>
       </view>
 
@@ -205,17 +219,22 @@ onLoad(() => {
   <!-- 用户操作 -->
   <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
     <view class="icons">
-      <button class="icons-button"><text class="icon-heart"></text>收藏</button>
+      <button class="icons-button">
+        <text class="icon-heart"></text>
+        收藏
+      </button>
       <button class="icons-button" open-type="contact">
-        <text class="icon-handset"></text>客服
+        <text class="icon-handset"></text>
+        客服
       </button>
       <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
-        <text class="icon-cart"></text>购物车
+        <text class="icon-cart"></text>
+        购物车
       </navigator>
     </view>
     <view class="buttons">
-      <view class="addcart" @tap="openSkuPopup(SKU_MODE.CART)"> 加入购物车 </view>
-      <view class="buynow" @tap="openSkuPopup(SKU_MODE.BUY)"> 立即购买 </view>
+      <view class="addcart" @tap="openSkuPopup(SKU_MODE.CART)"> 加入购物车</view>
+      <view class="buynow" @tap="openSkuPopup(SKU_MODE.BUY)"> 立即购买</view>
     </view>
   </view>
 </template>
@@ -235,6 +254,7 @@ page {
 .panel {
   margin-top: 20rpx;
   background-color: #fff;
+
   .title {
     display: flex;
     justify-content: space-between;
@@ -243,6 +263,7 @@ page {
     line-height: 1;
     padding: 30rpx 60rpx 30rpx 6rpx;
     position: relative;
+
     text {
       padding-left: 10rpx;
       font-size: 28rpx;
@@ -250,6 +271,7 @@ page {
       font-weight: 600;
       border-left: 4rpx solid #27ba9b;
     }
+
     navigator {
       font-size: 24rpx;
       color: #666;
@@ -273,13 +295,16 @@ page {
 /* 商品信息 */
 .goods {
   background-color: #fff;
+
   .preview {
     height: 750rpx;
     position: relative;
+
     .image {
       width: 750rpx;
       height: 750rpx;
     }
+
     .indicator {
       height: 40rpx;
       padding: 0 24rpx;
@@ -291,21 +316,26 @@ page {
       position: absolute;
       bottom: 30rpx;
       right: 30rpx;
+
       .current {
         font-size: 26rpx;
       }
+
       .split {
         font-size: 24rpx;
         margin: 0 1rpx 0 2rpx;
       }
+
       .total {
         font-size: 24rpx;
       }
     }
   }
+
   .meta {
     position: relative;
     border-bottom: 1rpx solid #eaeaea;
+
     .price {
       height: 130rpx;
       padding: 25rpx 30rpx 0;
@@ -314,9 +344,11 @@ page {
       box-sizing: border-box;
       background-color: #35c8a9;
     }
+
     .number {
       font-size: 56rpx;
     }
+
     .brand {
       width: 160rpx;
       height: 80rpx;
@@ -325,6 +357,7 @@ page {
       top: 26rpx;
       right: 30rpx;
     }
+
     .name {
       max-height: 88rpx;
       line-height: 1.4;
@@ -332,6 +365,7 @@ page {
       font-size: 32rpx;
       color: #333;
     }
+
     .desc {
       line-height: 1;
       padding: 0 20rpx 30rpx;
@@ -339,8 +373,10 @@ page {
       color: #cf4444;
     }
   }
+
   .action {
     padding-left: 20rpx;
+
     .item {
       height: 90rpx;
       padding-right: 60rpx;
@@ -350,15 +386,18 @@ page {
       position: relative;
       display: flex;
       align-items: center;
+
       &:last-child {
         border-bottom: 0 none;
       }
     }
+
     .label {
       width: 60rpx;
       color: #898b94;
       margin: 0 16rpx 0 10rpx;
     }
+
     .text {
       flex: 1;
       -webkit-line-clamp: 1;
@@ -369,15 +408,19 @@ page {
 /* 商品详情 */
 .detail {
   padding-left: 20rpx;
+
   .content {
     margin-left: -20rpx;
+
     .image {
       width: 100%;
     }
   }
+
   .properties {
     padding: 0 20rpx;
     margin-bottom: 30rpx;
+
     .item {
       display: flex;
       line-height: 2;
@@ -386,9 +429,11 @@ page {
       color: #333;
       border-bottom: 1rpx dashed #ccc;
     }
+
     .label {
       width: 200rpx;
     }
+
     .value {
       flex: 1;
     }
@@ -402,6 +447,7 @@ page {
     background-color: #f4f4f4;
     display: flex;
     flex-wrap: wrap;
+
     .goods {
       width: 340rpx;
       padding: 24rpx 20rpx 20rpx;
@@ -409,26 +455,31 @@ page {
       border-radius: 10rpx;
       background-color: #fff;
     }
+
     .image {
       width: 300rpx;
       height: 260rpx;
     }
+
     .name {
       height: 80rpx;
       margin: 10rpx 0;
       font-size: 26rpx;
       color: #262626;
     }
+
     .price {
       line-height: 1;
       font-size: 20rpx;
       color: #cf4444;
     }
+
     .number {
       font-size: 26rpx;
       margin-left: 2rpx;
     }
   }
+
   navigator {
     &:nth-child(even) {
       margin-right: 0;
@@ -451,8 +502,10 @@ page {
   justify-content: space-between;
   align-items: center;
   box-sizing: content-box;
+
   .buttons {
     display: flex;
+
     & > view {
       width: 220rpx;
       text-align: center;
@@ -461,20 +514,24 @@ page {
       color: #fff;
       border-radius: 72rpx;
     }
+
     .addcart {
       background-color: #ffa868;
     }
+
     .buynow,
     .payment {
       background-color: #27ba9b;
       margin-left: 20rpx;
     }
   }
+
   .icons {
     padding-right: 10rpx;
     display: flex;
     align-items: center;
     flex: 1;
+
     .icons-button {
       flex: 1;
       text-align: center;
@@ -485,10 +542,12 @@ page {
       font-size: 20rpx;
       color: #333;
       background-color: #fff;
+
       &::after {
         border: none;
       }
     }
+
     text {
       display: block;
       font-size: 34rpx;
---
+++ src/services/CartService.ts
@@ -0,0 +1,20 @@
+import HttpUtil from "@/utils/HttpUtil"
+import type { Result } from "@/types/global"
+import type { CartItem } from "@/types/cart"
+
+export default class CartService {
+  /**
+   * 加入购物车
+   * @param data skuId skuID, count: 商品数量
+   */
+  public static postMemberCartAPI = (data: {
+    skuId: string
+    count: number
+  }): Promise<Result<CartItem>> => {
+    return HttpUtil.http<CartItem>({
+      method: "POST",
+      url: "/member/cart",
+      data,
+    })
+  }
+}
